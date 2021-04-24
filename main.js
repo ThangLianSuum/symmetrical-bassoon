e3b78e4c6a824b6dbe8c79d7d79cb68adb972636
ThangLianSuum <75762682+ThangLianSuum@users.noreply.github.com>
2021-04-24 16:22:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+video.size(550, 500);
+
+canvas = createCanvas(550, 550);
+canvas.position(560,150);
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+    console.log('poseNet Is Initialized!')
+}
+
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+console.log(results);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWrist);
+
+        console.log("leftWristX = "+ leftWristX +" rightWristX = "+ rightWristX + " difference = " + difference);
+    }
+}
+
+function draw() {
+    document.getElementById("square_side").innerHTML = "width And Height of the text will be = "+ difference +"px";
+    fill('#f90093');
+    textSize(15)
+    text('According to all known laws', noseX, noseY, difference);
+}
